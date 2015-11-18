871a3f45f2d6c42a7a18f04b870f8c77874774ab
Akshay Kulkarni <akshaykul13@gmail.com>
2015-11-18 01:06:43 -0600

    Added flip switch to enable/disable extension

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+    // Initialize the on-off extension switch
+    chrome.storage.sync.get(['languagerEnabled'], function(data) {
+         $('#myonoffswitch').prop('checked', data.languagerEnabled);       
+    });    
 
-    $('#button').click(function() {
+    $('#myonoffswitch').click(function() {
         chrome.storage.sync.get(['languagerEnabled'], function(data) {
             chrome.storage.sync.set({'languagerEnabled': !data.languagerEnabled});       
         });        
