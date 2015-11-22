d533f4cc0569e7274ad1cf310652819562f09de0
Akshay Kulkarni <akshaykul13@gmail.com>
2015-11-22 17:33:12 -0600

    Adding check to see if word is already saved

--- extension/php/saveword.php
+++ extension/php/saveword.php
@@ -8,15 +8,22 @@ if(isset($_POST['jsonString'])){
 	$text = $json->{'text'};
 	$language = $json->{'language'};
 
-	$query = "Insert into words values ('',".$id.",'".$text."','".$language."')";
-	$query_run = mysqli_query($link, $query);
-	echo $query;
-	if($query_run){
-		error_log("Saved langauger word");
-	}else{
-		error_log("Error inserting save word");
-		die('Invalid query: ' . mysql_error());		
-	}
+	$check_query = "Select * from words where userid=".$id." and word='".$text."' and language='".$language."'";
+	$check_query_run = mysqli_query($link, $check_query);
+	if($check_query_run){
+		$check_query_num_rows = $check_query_run->num_rows;
+		if($check_query_num_rows == 0){
+			$query = "Insert into words values ('',".$id.",'".$text."','".$language."')";
+			$query_run = mysqli_query($link, $query);
+			echo $query;
+			if($query_run){
+				error_log("Saved langauger word");
+			}else{
+				error_log("Error inserting save word");
+				die('Invalid query: ' . mysql_error());		
+			}
+		}
+	}	
 }
 
 ?>
