1d3ed88de9d0e124a5c996fe2e3fbc68abb1c4c3
Akshay Kulkarni <akshaykul13@gmail.com>
2015-11-30 02:33:43 -0600

    Using google translate for word replacement
    Using google translate instead of bing

--- extension/scripts/inject/langauger.js
+++ extension/scripts/inject/langauger.js
@@ -388,59 +388,55 @@ Langauger.expandToWordBoundary = function(range){
 //============================================================================
 
 Langauger.wordReplacementQuiz = function(difficulty) {
-    var language = Langauger.config.target;
+    
     // Translate text
     function translate(from, to, text, cb) {
-      $.ajax({
-        url: 'https://api.microsofttranslator.com/V2/Ajax.svc/Translate?oncomplete=?&appId=68D088969D79A8B23AF8585CC83EBA2A05A97651&from=' + from + '&to=' + to + '&text=' + encodeURIComponent(text),
-        type: "GET",
-        success: function(data) {
-          cb(decodeURIComponent(data.substr(1, data.length - 2)));
-        }
-      });
+        cb(translateString(text, 'en', getLanguageCode(to)));
     }
 
     // Split Functions
-    function splitByWord(difficulty) {
+    function splitByWord(difficulty, language) {
       $('p').each(function() {
         var word = $(this).getWord(difficulty);
+        if(!word) {
+            return;
+        }
         var that = this;
         translate('en', language, word, function (translatedWord) {
           $(that).html($(that).html().replace(/<\/*.+?>/g, '').replace(new RegExp("\\b" + word + "\\b", 'i'), "<span class='translate_14385' style='background-color: #FFFAB0; color: #000000' data-original=\"" + word + "\">" + translatedWord + "</span>"));
-          $('.translate_14385').click(function() {
-            //playSpeech(translatedWord);
-          });
         });
       });
     }
 
-    function splitBySentence(difficulty, probability) {
+    function splitBySentence(difficulty, probability, language) {
       $('p').each(function() {
         var rand = Math.random() * 10;
         if (rand < probability) {
           var sentence = $(this).getSentence(difficulty);
+          if(!sentence) {
+            return;
+        }
           var that = this;
           translate('en', language, sentence, function (translatedSentence) {
-            $(that).html($(that).html().replace(/<\/*.+?>/g, '').replace(sentence, "<span class='translate_14385' style='background-color: #FFFAB0; color: #000000' data-original=\"" + sentence + "\">" + translatedSentence + "</span>"));
-            $('.translate_14385').click(function() {
-              //playSpeech($(this).text());
-            });
+            $(that).html($(that).html().replace(/<\/*.+?>/g, '').replace(sentence, "<span class='translate_14385' style='background-color: #FFFAB0; color: #000000' data-original=\"" + sentence + "\">" + translatedSentence + "</span>"));            
           });
         }
       });
     }
 
-    if (difficulty == 1) {
-      splitByWord('easy');
-    } else if (difficulty == 2) {
-      splitByWord('hard');
-    } else if (difficulty == 3) {
-      splitBySentence('easy', 4);
-    } else if (difficulty == 4) {
-      splitBySentence('medium', 6);
-    } else if (difficulty == 5) {
-      splitBySentence('hard', 11);
-    }
+    chrome.storage.sync.get(['langaugerTargetLang'], function(data) {
+        if (difficulty == 1) {
+          splitByWord('easy', data.langaugerTargetLang);
+        } else if (difficulty == 2) {
+          splitByWord('hard', data.langaugerTargetLang);
+        } else if (difficulty == 3) {
+          splitBySentence('easy', 4, data.langaugerTargetLang);
+        } else if (difficulty == 4) {
+          splitBySentence('medium', 6, data.langaugerTargetLang);
+        } else if (difficulty == 5) {
+          splitBySentence('hard', 11, data.langaugerTargetLang);
+        }
+    });    
 }
 
 //============================================================================
