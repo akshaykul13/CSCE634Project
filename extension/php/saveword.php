306620cede7ff3864031a9b6ae3ab0fc91c6d7a6
Akshay Kulkarni <akshaykul13@gmail.com>
2015-12-01 11:45:35 -0600

    Updating last modified for word when translated

--- extension/php/saveword.php
+++ extension/php/saveword.php
@@ -12,18 +12,20 @@ if(isset($_POST['jsonString'])){
 	$check_query_run = mysqli_query($link, $check_query);
 	if($check_query_run){
 		$check_query_num_rows = $check_query_run->num_rows;
+		date_default_timezone_set("America/Chicago");
+		$lastmodified = date("Y-n-j H:i:s");
 		if($check_query_num_rows == 0){
-			date_default_timezone_set("America/Chicago");
-			$lastmodified = date("Y-n-j H:i:s");
 			$query = "Insert into words values ('',".$id.",'".$text."','".$language."',0,'".$lastmodified."')";
-			$query_run = mysqli_query($link, $query);
-			echo $query;
-			if($query_run){
-				error_log("Saved langauger word");
-			}else{
-				error_log("Error inserting save word");
-				die('Invalid query: ' . mysql_error());		
-			}
+		} else {
+			$query = "Update words set lastmodified='".$lastmodified."' where userid=".$id." and word='".$text."' and language='".$language."'";
+		}			
+		$query_run = mysqli_query($link, $query);
+		echo $query;
+		if($query_run){
+			error_log("Saved langauger word");
+		}else{
+			error_log("Error inserting save word");
+			die('Invalid query: ' . mysql_error());		
 		}
 	}	
 }
